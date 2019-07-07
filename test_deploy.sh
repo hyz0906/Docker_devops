c9f470faf015bdc97dac95b333470275c1accbd8
Yuzhe Han <hyz0906@gmail.com>
2019-07-07 11:56:21 +0800

    Update test_deploy.sh

--- test_deploy.sh
+++ test_deploy.sh
@@ -4,6 +4,6 @@ git clone https://www.github.com/hyz0906/tw_homework.git
 
 cd tw_homework
 docker build -t test -f docker/test/Dockerfile .
-
+docker rm -f test_env
 docker run -d --name=test_env --net=host test
 echo "You can access the portal from http://localhost and call the API on localhost:8000 now"
